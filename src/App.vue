437809cb1691a7c44ed227f5de6e4eb8b49cbd74
Luiza <luitays@mail.ru>
2019-06-05 09:27:48 +0300

    navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <v-app>
+    <app-navigation></app-navigation>
     <v-content transition="slide-x-transition">
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
-<style lang="scss">
-</style>
+<script>
+import AppNavigation from '@/components/AppNavigation';
+export default {
+  name: 'App',
+  components: {
+    AppNavigation
+  }
+};
+</script>
+
+<style></style>
